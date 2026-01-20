f1f39634250467812050b34353240f01e60db7bb
jhfated6 <jh.cristo06@hotmail.com>
2026-01-20 13:16:52 -0300

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -13,13 +13,13 @@ export function createClient() {
     env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: { path?: string; domain?: string; maxAge?: number; secure?: boolean; httpOnly?: boolean; sameSite?: "lax" | "strict" | "none" }) {
           cookieStore.set({ name, value, ...options });
         },
-        remove(name, options) {
+        remove(name: string, options: { path?: string; domain?: string; maxAge?: number; secure?: boolean; httpOnly?: boolean; sameSite?: "lax" | "strict" | "none" }) {
           cookieStore.set({ name, value: "", ...options });
         }
       }
